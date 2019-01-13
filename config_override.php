aca92f0ff29a7f09b6cb2841234040232a95ebf0
nguyenthaclong <thaclong.nguyen@gmail.com>
2019-01-13 22:15:43 +0100

    sugarDev: Init master

---
+++ config_override.php
@@ -0,0 +1,7 @@
+<?php
+/***CONFIGURATOR***/
+$sugar_config['default_currency_iso4217'] = 'EUR';
+$sugar_config['default_currency_name'] = 'Euro';
+$sugar_config['default_currency_symbol'] = '€';
+$sugar_config['default_number_grouping_seperator'] = ' ';
+/***CONFIGURATOR***/
---
+++ custom/modules/Accounts/language/en_us.lang.php
@@ -0,0 +1,5 @@
+<?php
+// created: 2018-12-24 21:51:05
+$mod_strings = array (
+  'LBL_EDITVIEW_PANEL1' => 'New Panel 1',
+);
